7ac966c00e1fc6d63ce61046eb7b796529c91b7a
Vishnu Rajan Tejus <vrtejus@gmail.com>
2023-06-18 03:36:30 -0700

    finish summarization api

--- Backend/functions/main.py
+++ Backend/functions/main.py
@@ -9,6 +9,10 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import openai
+
+openai.api_key = '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'
+
 initialize_app()
 
 @https_fn.on_request()
@@ -21,3 +25,20 @@ def on_request_example(req: https_fn.Request) -> https_fn.Response:
     except Exception as e:
         print(e)
         return https_fn.Response("Error connecting to database")
+    
+@https_fn.on_request()
+def summarization(req: https_fn.Request) -> https_fn.Response:
+    try:
+        transcript = req.get_json().get("transcript")
+        completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "Summarize this transcript from the perspective of a healthcare practitioner or patient. If not enough information is provided, ask the patient for more details."},
+                {"role": "user", "content": transcript}
+            ]
+        )
+        print(completion.choices[0].message.content)
+        return https_fn.Response(completion.choices[0].message.content)
+    except Exception as e:
+        print(e)
+        return https_fn.Response("Error with summarization")
