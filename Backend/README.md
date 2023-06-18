ec6811325ab04de65d25548a161761e7f224a1aa
Vishnu Rajan Tejus <vrtejus@gmail.com>
2023-06-17 22:12:50 -0700

    add README for backend

---
+++ Backend/README.md
@@ -0,0 +1,9 @@
+#Run Firebase Emulators
+
+> Make sure you have installed python3.11.
+
+`cd functions`
+
+`virtualenv -p `which python3.11` ./venv`
+
+`firebase emulators:start`
--- Backend/functions/main.py
+++ Backend/functions/main.py
@@ -5,9 +5,8 @@
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+initialize_app()
+
+@https_fn.on_request()
+def on_request_example(req: https_fn.Request) -> https_fn.Response:
+    return https_fn.Response("Hello world!")
