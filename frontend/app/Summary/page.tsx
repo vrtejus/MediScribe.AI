7b22e473e75bdf75474ab2adae49cca9477bc4be
Vishnu Rajan Tejus <vrtejus@gmail.com>
2023-06-30 16:24:35 -0700

    fixed the bug in the index page

--- frontend/app/Summary/page.tsx
+++ frontend/app/Summary/page.tsx
@@ -3,25 +3,8 @@
 import { useState } from "react"
 import Link from "next/link"
 import TranscriptionStore from "@/stores/TranscriptionStore"
-import { MicrophoneIcon } from "@heroicons/react/24/solid"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-import { AlignCenter } from "lucide-react"
 
 export default function IndexPage() {
-  const [isTranscribing, setIsTranscribing] = useState(false) // Add a state to track if transcription is in progress
-
-  const handleTranscriptionClick = async () => {
-    setIsTranscribing(true) // Set the state to indicate transcription is in progress
-    const mediaStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-    })
-    const audioTrack = mediaStream.getAudioTracks()[0]
-    await TranscriptionStore.startTranscription(audioTrack)
-    setIsTranscribing(false) // Reset the state after transcription is complete
-  }
-
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex flex-col items-center gap-2">
@@ -29,13 +12,14 @@ export default function IndexPage() {
           Summary <br className="hidden sm:inline" />
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-        your post medical examination summary
+          your post medical examination summary
         </p>
         <p className="max-w-[700px] text-sm text-muted-foreground">
-        Next Steps: Get a blood test and write down any experienced symptoms until next visit
+          Next Steps: Get a blood test and write down any experienced symptoms
+          until next visit
         </p>
         <p className="max-w-[700px] text-sm text-muted-foreground">
-        Follow up visit scheduled for August 23, 2023
+          Follow up visit scheduled for August 23, 2023
         </p>
       </div>
     </section>
